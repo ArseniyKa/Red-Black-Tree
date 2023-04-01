3f98adfb5e81b89796bc3a674d999ba7e762e69c
Arseniy <arseniykapkov@gmail.com>
2023-04-01 12:26:43 +0300

    ready insert and find

--- main.cpp
+++ main.cpp
@@ -3,112 +3,15 @@
 #include <istream>
 #include <stdint.h>
 
-template <typename T, typename M> class Node {
-public:
-  //    Node() { qDebug() << "constructor was called"; };
-  //    ~Node() { qDebug() << "destructor was called"; }
-  //    Node(const Node &other) {
-  //    qDebug() << "copy constructor was called";
-  //  }
-  //    Node &operator=(const Node &other) {
-  //    qDebug() << "copy assignment was called";
-  //  }
-
-  void insert(T key, M value) {
-
-    // only for root
-    if (parent_ == nullptr && left_ == nullptr && right_ == nullptr) {
-      InitRoot(key, value);
-      return;
-    }
-
-    if (key < key_) {
-      LeftInsert(key, value);
-    } else if (key > key_) {
-      RightInsert(key, value);
-    } else {
-      value_ = value;
-    }
-  }
-
-  static std::int64_t size() { return size_; }
-
-  M find(T key) {
-    M answer = '888';
-    // only for root
-    if (size_ == 0) {
-      qDebug() << "the tree is empty";
-      return answer;
-    } else if (parent_ != nullptr && left_ == nullptr && right_ == nullptr) {
-      qDebug() << "not found the key";
-      return answer;
-    }
-
-    if (key < key_ && left_ != nullptr) {
-      answer = left_->find(key);
-    } else if (key > key_ && right_ != nullptr) {
-      answer = right_->find(key);
-      //    } else if (key < key_ && left_ == nullptr) {
-      //      qDebug() << "not found left key";
-      //    } else if (key > key_ && right_ == nullptr) {
-      //      qDebug() << "not found right key";
-    } else if (key == key_) {
-      return value_;
-    }
-
-    return answer;
-  }
-
-  // private:
-  void LeftInsert(T key, M value) {
-    Node<T, M> *node = left_;
-
-    // insert in that place
-    if (node == nullptr) {
-      InitNode(key, value, node);
-      return;
-    }
-
-    node->insert(key, value);
-  }
-
-  void RightInsert(T key, M value) {
-    Node<T, M> *node = right_;
-
-    // insert in that place
-    if (node == nullptr) {
-      InitNode(key, value, node);
-      return;
-    }
-    node->insert(key, value);
-  }
-
-  void InitNode(T key, M value, Node<T, M> *node) {
-    node = new Node<T, M>;
-    node->key_ = key;
-    node->value_ = value;
-    node->parent_ = this;
-    size_++;
-  }
-
-  void InitRoot(T key, M value) {
-    key_ = key;
-    value_ = value;
-    size_++;
-  }
-
+template <typename T, typename M> struct Node {
   T key_; // ключ узла
   M value_;
   Node<T, M> *left_{nullptr}; // указатель на левого потомка
   Node<T, M> *right_{nullptr}; // указатель на правого потомка
   Node<T, M> *parent_{nullptr}; // указатель на предка
-  static std::int64_t size_;
 };
 
-template <typename T, typename M> std::int64_t Node<T, M>::size_ = 0;
-
 template <typename T, typename M> class BinaryTree {
-
 public:
   BinaryTree() { qDebug() << "constructor was called"; };
   ~BinaryTree() { qDebug() << "destructor was called"; }
@@ -121,12 +24,76 @@ public:
 
   void insert(T key, M value) {
     if (root_ == nullptr) {
-      root_ = new Node<T, M>;
-      root_->key_ = key;
+      InitNode(key, value, root_, nullptr);
+      return;
+    }
+
+    if (key < root_->key_) {
+      SubInsert(key, value, root_->left_, root_);
+    } else if (key > root_->key_) {
+      SubInsert(key, value, root_->right_, root_);
+    } else {
       root_->value_ = value;
     }
   }
 
+  std::int64_t size() const { return size_; }
+
+  M find(T key) {
+    if (root_ == nullptr) {
+      throw std::runtime_error("the tree is empty");
+    }
+
+    if (key < root_->key_) {
+      return SubFind(key, root_->left_);
+    } else if (key > root_->key_) {
+      return SubFind(key, root_->right_);
+    } else if (key == root_->key_) {
+      return root_->value_;
+    } else {
+      throw std::runtime_error("undefined behavior in find()");
+    }
+  }
+
+private:
+  void SubInsert(T key, M value, Node<T, M> *&node, Node<T, M> *parent) {
+    if (node == nullptr) {
+      InitNode(key, value, node, parent);
+      return;
+    }
+
+    if (key < node->key_) {
+      SubInsert(key, value, node->left_, node);
+    } else if (key > node->key_) {
+      SubInsert(key, value, node->right_, node);
+    }
+  }
+
+  M SubFind(T key, Node<T, M> *node) {
+    if (node == nullptr) {
+      throw std::runtime_error("the node is nullptr");
+    }
+
+    if (key < node->key_) {
+      return SubFind(key, node->left_);
+    } else if (key > node->key_) {
+      return SubFind(key, node->right_);
+    } else if (key == node->key_) {
+      return node->value_;
+    } else {
+      throw std::runtime_error("undefined behavior in SubFind()");
+    }
+  }
+
+  void InitNode(T key, M value, Node<T, M> *&node, Node<T, M> *parent) {
+    node = new Node<T, M>;
+    node->key_ = key;
+    node->value_ = value;
+    node->parent_ = parent;
+    size_++;
+  }
+
+  std::int64_t size_{0};
   Node<T, M> *root_{nullptr};
 };
 
@@ -134,9 +101,9 @@ int main(int argc, char *argv[]) {
   BinaryTree<int, char> tree;
   tree.insert(20, 'd');
   tree.insert(30, 'e');
-  tree.insert(15, 'a');
   tree.insert(7, 'l');
-  qDebug() << tree.size();
+  tree.insert(15, 'a');
+  qDebug() << "tree size" << tree.size();
   auto ans = tree.find(15);
   qDebug() << ans;
   return 0;
